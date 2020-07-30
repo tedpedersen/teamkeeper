f4cb22342df573a5f7a6f45e89bb8fa4ec14ec33
Ted Pedersen <tedpedersen@me.com>
2020-07-30 15:21:14 -0700

    logic for more employees

--- index.js
+++ index.js
@@ -3,62 +3,63 @@ var inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-// class Employee{
-//     constructor(name, id, email){
-//         this.name = name;
-//         this.id = id;
-//         this.email = email;
-//     }
+class Employee{
+    constructor(name, id, email, role){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.role = role;
+    }
 
-//     getName(){
-//         return this.name;
-//     }
+    getName(){
+        return this.name;
+    }
 
-//     getId(){
-//         return this.id;
-//     }
+    getId(){
+        return this.id;
+    }
 
-//     getEmail(){
-//         return this.email;
-//     }
+    getEmail(){
+        return this.email;
+    }
 
-//     getRole(){
-//         return "Employee";
-//     }
-// }
+    getRole(){
+        return this.role;
+    }
+}
 
-// class Manager extends Employee{
-//     constructor(officeNumber){
-//         this.officeNumber = officeNumber;
-//     }
-//     getRole(){
-//         return "Manager";
-//     }  
-// }
+class Manager extends Employee{
+    constructor(officeNumber){
+        this.officeNumber = officeNumber;
+    }
+    getRole(){
+        return "Manager";
+    }  
+}
 
-// class Engineer extends Employee{
-//     constructor(github){
-//         this.github = github;
-//     }
-//     getGithub(){
-//         return this.github;
-//     }
-//     getRole(){
-//         return "Engineer";
-//     }  
-// }
+class Engineer extends Employee{
+    constructor(github){
+        this.github = github;
+    }
+    getGithub(){
+        return this.github;
+    }
+    getRole(){
+        return "Engineer";
+    }  
+}
 
-// class Intern extends Employee{
-//     constructor(school){
-//         this.school = school;
-//     }
-//     getSchool(){
-//         return this.school;
-//     }
-//     getRole(){
-//         return "Intern";
-//     }
-// }
+class Intern extends Employee{
+    constructor(school){
+        this.school = school;
+    }
+    getSchool(){
+        return this.school;
+    }
+    getRole(){
+        return "Intern";
+    }
+}
 
 // inquirer
 //   .prompt({
@@ -83,36 +84,65 @@ function promptUser() {
         choices: [
             "Manager",
             "Engineer",
-            "Intern"
+            "Intern",
         ],
         name: "role"
         },
         {
         type: 'input',
         name: 'name',
-        message: 'Please enter the their name:',
+        message: 'Please enter the employee name:',
         },
         {
         type: 'input',
         name: 'email',
-        message: 'Please enter their email address:',
+        message: 'Please enter the employee email address:',
+        },
+        {
+        type: 'confirm',
+        name: 'askAgain',
+        message: 'Would you like to enter another employee? (just hit enter for YES)?',
+        default: true,
         },
     ]);
-} 
+
+}
+
 
 const generateHtml = (response) => {
+    let theName = new Employee(response.name);
+    let name = theName.name;
+
+
+    var card = `<div class="uk-card uk-card-default uk-card-body uk-width-1-2@m">
+<h3 class="uk-card-title">${response.role}</h3>
+<p>Name: ${name}</p>
+<p>Email <a href="mailto:${response.email}">${response.email}</a></p></div>`
+
+
     return `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teamkeeper</title>
-    ${response.name}
-    ${response.role}
-    ${response.email}
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/css/uikit.min.css" />
+
 </head>
 <body>
-
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-center">
+        <a href="#" class="uk-navbar-item uk-logo">TEAMKEEPER</a>
+    </div>
+</nav>
+<main class="uk-container">
+<br />
+${card}
+</main>
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.5/dist/js/uikit-icons.min.js"></script>
 </body>
 </html>`
 }
@@ -120,18 +150,26 @@ const generateHtml = (response) => {
 //create the html file after questions
 const writeFile = util.promisify(fs.writeFile);
 
-//wait for the answers, then create the markdown
+//wait for the answers, then create the html
 async function init() {
+
     try {
         const response = await promptUser();
 
         const html = generateHtml(response);
+        if (response.askAgain === false){
 
         await writeFile("output/index.html", html);
-        console.log("Success! Your team's html file has been created in the output dir.");
+        console.log("Success! Your team's index.html file has been created in the /output/ dir.");
+        console.log(response);
+        }
+        else{
+            promptUser();
+        }
     } catch (err){
         console.log(err);
     }
 }
+
 
 init();
